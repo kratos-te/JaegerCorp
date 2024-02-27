2bc8afc17f5f5ecdf902804c956624552aa90101
kratos-te <michaelgordon.kratos@gmail.com>
2024-02-28 05:30:40 +0900

    fix: build error

--- src/app/api/auth/[...nextauth].js
+++ src/app/api/auth/[...nextauth].js
@@ -8,32 +8,32 @@ export default NextAuth({
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
     }),
   ],
-  callbacks: {   
-    async jwt({ token, account, profile, session }) {
-      console.log("token", token);
-      console.log("profile",profile);
-      console.log("account", account);
-      console.log("session", session);
-      if (account) {
-        if (account.provider === 'twitter') {
-          token["userProfile"] = {
-            twitterHandle: profile.name,
-            userID: profile.id,
-          };
-        }
+//   callbacks: {   
+//     async jwt({ token, account, profile, session }) {
+//       console.log("token", token);
+//       console.log("profile",profile);
+//       console.log("account", account);
+//       console.log("session", session);
+//       if (account) {
+//         if (account.provider === 'twitter') {
+//           token["userProfile"] = {
+//             twitterHandle: profile.name,
+//             userID: profile.id,
+//           };
+//         }
 
-        token["credentials"] = {
-          accessToken: account.provider ==='twitter' ? account.oauth_token : account.access_token,
-          refreshToken: account.provider ==='twitter' ? account.oauth_token_secret: account.refresh_token,
-        };
-      }
-      if (!token.credentials.accessToken || !token.credentials.refreshToken) {
-        token.userProfile = {};
-        token.credentials = {};
-      }
-      return token;
-    },
-  },
+//         token["credentials"] = {
+//           accessToken: account.provider ==='twitter' ? account.oauth_token : account.access_token,
+//           refreshToken: account.provider ==='twitter' ? account.oauth_token_secret: account.refresh_token,
+//         };
+//       }
+//       if (!token.credentials.accessToken || !token.credentials.refreshToken) {
+//         token.userProfile = {};
+//         token.credentials = {};
+//       }
+//       return token;
+//     },
+//   },
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     error: "/", // Error code passed in query string as ?error=
